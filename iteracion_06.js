f0a27c3a1cfd367edb8b25789222e12724879e65
Ruben <ruben.infantes@gmail.com>
2022-03-09 17:34:15 +0100

    iteraction#6 complete

--- iteracion_06.js
+++ iteracion_06.js
@@ -13,3 +13,22 @@ const movies = [
 
 const foundMovie = movies.find(item => item.date == 2010);
 console.log(foundMovie);
+
+// Parte 6.3
+
+const aliens = [
+    { name: 'Zalamero', planet: 'Eden', age: 4029 },
+    { name: 'Paktu', planet: 'Andromeda', age: 32 },
+    { name: 'Cucushumushu', planet: 'Marte', age: 503021 }
+];
+const mutations = [
+    { name: 'Porompompero', description: 'Hace que el alien pueda adquirir la habilidad de tocar el tambor' },
+    { name: 'Fly me to the moon', description: 'Permite volar, solo y exclusivamente a la luna' },
+    { name: 'Andando que es gerundio', description: 'Invoca a un señor mayor como Personal Trainer' }
+];
+
+const alien = aliens.find(item => item.name === 'Cucushumushu');
+const mutation = mutations.find(item => item.name === 'Porompompero')
+
+const newAlien = { ...alien, mutation: { ...mutation } };
+console.log(newAlien);
